b8097967442735e2dc0adc02676edd4db74b430f
Renyyyyy <reny.zol.2019@gmail.com>
2025-07-10 20:49:31 +0300

    post delete fixes

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,45 +1,58 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
-import { CreatePostDto } from './dto/create-post.dto';
-import { PostsService } from './posts.service';
-import { Post as PostModel} from './posts.model';
-import { User } from 'src/users/users.model';
-import { DeletePostDto } from './dto/delete-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { PostsService } from "./posts.service";
+import { Post as PostModel } from "./posts.model";
+import { User } from "src/users/users.model";
+import { DeletePostDto } from "./dto/delete-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
-@Controller('posts')
+@Controller("posts")
 export class PostsController {
+  constructor(private postService: PostsService) {}
 
-    constructor(private postService: PostsService){}
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  create(
+    @Body() dto: CreatePostDto,
+    @Req() req: Request & { user: User }
+  ): Promise<PostModel> {
+    return this.postService.create(dto, req.user);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    create(@Body() dto: CreatePostDto, @Req() req: Request & { user: User }): Promise<PostModel> {
-        return this.postService.create(dto, req.user);
-    }
-    
-    @Put()
-    @UseGuards(JwtAuthGuard)
-    async update(
-        @Body() dto: UpdatePostDto,
-        @Req() req: Request & { user: User }
-    ): Promise<void> {
-        await this.postService.update(dto, req.user);
-    }
+  @Put()
+  @UseGuards(JwtAuthGuard)
+  async update(
+    @Body() dto: UpdatePostDto,
+    @Req() req: Request & { user: User }
+  ): Promise<void> {
+    await this.postService.update(dto, req.user);
+  }
 
-    @Delete()
-    @UseGuards(JwtAuthGuard)
-    @HttpCode(204)
-    async delete(
-        @Body() dto: DeletePostDto,
-        @Req() req: Request & { user: User }
-    ): Promise<void> {
-        await this.postService.delete(dto, req.user);
-    }
+  @Delete(":id")
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(204)
+  async delete(
+    @Param("id") id: number,
+    @Req() req: Request & { user: User }
+  ): Promise<void> {
+    await this.postService.delete(id, req.user);
+  }
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    async getPostById(@Param('id') id: number): Promise<PostModel> {
-        return this.postService.getPostById(id);
-    }
+  @Get(":id")
+  @UseGuards(JwtAuthGuard)
+  async getPostById(@Param("id") id: number): Promise<PostModel> {
+    return this.postService.getPostById(id);
+  }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,76 +1,83 @@
-import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
-import { Post } from './posts.model';
-import { CreatePostDto } from './dto/create-post.dto';
-import { User } from 'src/users/users.model';
-import { DeletePostDto } from './dto/delete-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { Like } from 'src/likes/likes.model';
-import { Comment } from 'src/comments/comments.model';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Post } from "./posts.model";
+import { CreatePostDto } from "./dto/create-post.dto";
+import { User } from "src/users/users.model";
+import { DeletePostDto } from "./dto/delete-post.dto";
+import { UpdatePostDto } from "./dto/update-post.dto";
+import { Like } from "src/likes/likes.model";
+import { Comment } from "src/comments/comments.model";
 
 @Injectable()
 export class PostsService {
+  constructor(@InjectModel(Post) private postRepository: typeof Post) {}
 
-    constructor(@InjectModel(Post) private postRepository: typeof Post){}
+  async create(dto: CreatePostDto, user: User): Promise<Post> {
+    const post = await this.postRepository.create({ ...dto, userId: user.id });
+    return post;
+  }
 
-    async create(dto: CreatePostDto, user: User):Promise<Post>{
-        const post = await this.postRepository.create({...dto, userId: user.id})
-        return post;
+  async update(dto: UpdatePostDto, user: User): Promise<void> {
+    const post = await this.postRepository.findByPk(dto.id);
+
+    if (!post) {
+      throw new NotFoundException("Post not found");
     }
 
-    async update(dto: UpdatePostDto, user: User): Promise<void> {
-        const post = await this.postRepository.findByPk(dto.id);
-        
-        if (!post) {
-            throw new NotFoundException('Post not found');
-        }
-        
-        if (post.userId !== user.id) {
-            throw new ForbiddenException('You may only update your own posts');
-        }
-        
-        await post.update({ 
-            title: dto.title, 
-            content: dto.content 
-        });
-    }
-    
-    async delete(dto: DeletePostDto, user: User): Promise<void> {
-        const post = await this.postRepository.findByPk(dto.id);
-        
-        if (!post) {
-            throw new NotFoundException('Post not found');
-        }
-        
-        if (post.userId !== user.id) {
-            throw new ForbiddenException('You may only delete your own posts');
-        }
-        
-        await post.destroy();
+    if (post.userId !== user.id) {
+      throw new ForbiddenException("You may only update your own posts");
     }
 
-    async getPostById(id: number): Promise<Post> {
-        return this.postRepository.findByPk(id, {
-            include: [
-                { 
-                    model: User,
-                    attributes: ['id', 'nickname', 'login'] 
-                },
-                { 
-                    model: Comment,
-                    include: [{
-                        model: User,
-                        attributes: ['id', 'nickname']
-                    }]
-                },
-                { 
-                    model: Like,
-                    include: [{
-                        model: User,
-                        attributes: ['id', 'nickname']
-                    }]
-                }
-            ]
-        });
+    await post.update({
+      title: dto.title,
+      content: dto.content,
+    });
+  }
+
+  async delete(id: number, user: User): Promise<void> {
+    const post = await this.postRepository.findByPk(id);
+
+    if (!post) {
+      throw new NotFoundException("Post not found");
     }
+
+    if (post.get("userId") !== user.id) {
+      throw new ForbiddenException("You may only delete your own posts");
+    }
+
+    await post.destroy();
+  }
+
+  async getPostById(id: number): Promise<Post> {
+    return this.postRepository.findByPk(id, {
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname", "login"],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+          ],
+        },
+        {
+          model: Like,
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+          ],
+        },
+      ],
+    });
+  }
 }
